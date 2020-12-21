ee1116a171acc6bfefeba83de04fb89b010edb30
Jordi <jorllorber@gmail.com>
2020-12-21 19:42:50 +0100

    Primer acercamiento al mundo

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float velocity = 5f;
+
+    float verticalMov;
+    float horizontalMov;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        verticalMov = Input.GetAxisRaw("Vertical");
+        horizontalMov = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(new Vector3(horizontalMov, verticalMov) * velocity * Time.fixedDeltaTime);
+    }
+}
---
+++ Assets/RoomSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawner : MonoBehaviour
+{
+    public int openingDirection;
+
+    private void Update()
+    {
+        if(openingDirection == 1)
+        {
+            //Need BOTTOM door
+        }
+        else if (openingDirection == 2)
+        {
+            //Need TOP door
+        }
+        else if (openingDirection == 3)
+        {
+            //Need LEFT door
+        }
+        else if (openingDirection == 4)
+        {
+            //Need RIGHT door
+        }
+    }
+}
